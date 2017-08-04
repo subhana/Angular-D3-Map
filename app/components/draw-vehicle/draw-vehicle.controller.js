3ac0e281d08c82db8b05bbf310ddd66a6f7badfd
Subhana Khan <skhan@c.systemsinmotion.com>
2017-08-04 15:00:51 -0400

    Added dropdown for route selection

--- app/components/draw-vehicle/draw-vehicle.controller.js
+++ app/components/draw-vehicle/draw-vehicle.controller.js
@@ -5,8 +5,12 @@ drawVehicleModule.controller('DrawVehicleController', [
     function drawVehicleController($scope, $rootScope, Notification, $timeout, RouteService) {
 
         RouteService.get().then(function(routes) {
-            console.log("routes");
-            console.log(routes);
+            $scope.routes = routes.route;
         });
+
+        $scope.getVehicles = function() {
+            console.log("selected route");
+            console.log($scope.selectedRoute);
+        }
     }
 ]);
