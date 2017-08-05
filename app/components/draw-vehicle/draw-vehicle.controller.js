a6f8c2bd1017d228628105355e346ed7942c6f06
Subhana Khan <skhan@c.systemsinmotion.com>
2017-08-04 23:23:28 -0400

    created resource class for vehicle

--- app/components/draw-vehicle/draw-vehicle.controller.js
+++ app/components/draw-vehicle/draw-vehicle.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 drawVehicleModule.controller('DrawVehicleController', [
-    '$scope', '$rootScope', 'Notification', '$timeout', 'RouteService',
-    function drawVehicleController($scope, $rootScope, Notification, $timeout, RouteService) {
+    '$scope', '$timeout', 'RouteService','VehicleService',
+    function drawVehicleController($scope, $timeout, RouteService, VehicleService) {
 
         RouteService.get().then(function(routes) {
             $scope.routes = routes.route;
@@ -11,6 +11,10 @@ drawVehicleModule.controller('DrawVehicleController', [
         $scope.getVehicles = function() {
             console.log("selected route");
             console.log($scope.selectedRoute);
-        }
+            VehicleService.get($scope.selectedRoute.tag).then(function(vehicles) {
+                console.log("vehicles");
+                console.log(vehicles);
+            });
+        };
     }
 ]);
